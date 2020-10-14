9683a25c293076f7638dce8770f537790afb5a6d
matías <matiasmaldonado400@gmail.com>
2020-10-13 22:58:21 -0300

    no entres si eres un purista del codigo xd

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <html lang="en">
 <head>
   <title>PHP crud</title>
@@ -8,14 +9,42 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
+<!--body init-->
 <body>
-
+<?php
+$user = "el usuario lo deve introducir desde el input";
+$mensaje = "hola wenas esto es php";
+?>
+<!--logo-->
 <div class="jumbotron text-center">
   <h1>MathNet</h1>
 </div>
+<!--mensaje-->
 <div class="container"><p class="font-weight-bolder">ingrese el mensaje here:</p></div>
-<div class= "" >
-
+<div class="container">
+  <label for="comment">mensaje:</label>
+  <textarea class="form-control" rows="5" id="comment"></textarea>
 </div>
+<div class= "container">
+<div class="table-responsive-sm">
+<table class="table table-striped">
+    <thead>
+      <tr>
+        <th>user</th>
+        <th>message</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>random</td>
+            <td><?= $mensaje ?></td>
+        </tr>
+        <tr>
+            <td>mathias</td>
+            <td>hola que talka</td>
+        </tr>
+    </tbody>
+    </div>
+    </div>
 </body>
 </html>
